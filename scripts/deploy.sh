dbb4b853291b4def6ab3c665da9a66da9fd60df0
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-05-02 21:58:26 -0400

    Minor improvements

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-# recreate the bot docker image
+usage="usage: $0 [-p|--prod]"
+if [ "$#" -gt 1 ]; then echo $usage && exit 1; fi
+if [ "$1" != "-p" ] && [ "$1" != "--prod" ] && [ "$1" != "" ]; then
+    echo $usage && exit 1
+fi
+
+# update the bot
+if [ $1 ]; then
+    echo "production deployment"
+    git fetch --all
+    git pull --rebase --auto-stash
+fi
+
+# recreate the image
+docker-compose pull
 docker-compose build --no-cache
 docker-compose up --detach
